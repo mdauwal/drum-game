aa07164176461fa029175c67e4c7d52f45a82ac5
mdauwal <pricelezzchap@gmail.com>
2024-08-24 09:00:20 +0100

    keyboard event added

--- script.js
+++ script.js
@@ -1,28 +1,75 @@
+//Detecting button pressed
+
 const allBtn = document.querySelectorAll(".drum").length;
-for (let i = 0; i< allBtn; i++) {
+for (var i = 0; i< allBtn; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        alert("I got clicked!");
-        firstBtn
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML)
+        
+
+        
     });
 }
 
-function add(numb1, numb2) {
-    return (numb1 + numb2);
-}
+//Detecting keyboard pressed
 
-function subtract(numb1, numb2) {
-    return (numb1 - numb2);
-}
+document.addEventListener("keydown", function(event){
+    makeSound(event.key)
+})
 
-function multiply(numb1, numb2) {
-    return(numb1 * numb2);
-}
-function divide(numb1, numb2) {
-    return(numb1 / numb2);
-}
-function calculator(numb1, numb2, operator) {
-    return operator(numb1, numb2,add)
+function makeSound (key) {
+    switch (key) {
+        case "l":
+            var audio = new Audio('./sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('./sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('./sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('./sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio('./sounds/crash.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('./sounds/snare.mp3');
+            audio.play();
+            break;
+        case "w":
+            var audio = new Audio('./sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        default: console.log(buttonInnerHTML);
+            break;
+    }
 
 }
-calculator(2,4,add);
+// 
+// function add(numb1, numb2) {
+//     return (numb1 + numb2);
+// }
+
+// function subtract(numb1, numb2) {
+//     return (numb1 - numb2);
+// }
+
+// function multiply(numb1, numb2) {
+//     return(numb1 * numb2);
+// }
+// function divide(numb1, numb2) {
+//     return(numb1 / numb2);
+// }
+// function calculator(numb1, numb2, operator) {
+//     return operator(numb1, numb2,add)
+
+// }
+// calculator(2,4,add);
